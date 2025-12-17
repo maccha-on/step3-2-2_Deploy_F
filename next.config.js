f5e8e503c20c508cab5a5ec7aaee7bb9c8200638
maccha-on <masato.ohtani.tech0@gmail.com>
2025-12-17 12:56:49 +0900

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    // ↑追加部分
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
